407afb7c4140c5c4ff0da9cfaa5a864a7b39da9c
Alexander A. Kropotin <ololx@icloud.com>
2022-01-11 18:36:23 +0500

    feature(FinalExample.java) add example for final annotation

---
+++ src/main/java/io/github/cranberry/examples/FinalExample.java
@@ -0,0 +1,24 @@
+package io.github.cranberry.examples;
+
+import io.github.ololx.cranberry.data.modifier.annotation.Final;
+
+import java.util.Set;
+
+/**
+ * project cranberry-examples
+ * created 2022-01-11 18:29
+ *
+ * @author Alexander A. Kropotin
+ */
+public class FinalExample {
+
+    public int finalLocalVariableExample() {
+        @Final Set<Integer> finalLocalVariable = Set.of(1, 2, 3, 4);
+
+        return finalLocalVariable.stream().reduce(Integer::sum).orElse(0);
+    }
+
+    public int finalMethodParamExample(@Final Set<Integer> finalMethodParam) {
+        return finalMethodParam.stream().reduce(Integer::sum).orElse(0);
+    }
+}
