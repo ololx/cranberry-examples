3c2814cd48bbef771fd5cd9df370d85b86fef816
ololx <ololx@icloud.com>
2022-02-01 16:27:16 +0500

    feature(NotBlankConstructorParamExamples.java) add NotBlank Constructor Examples

---
+++ src/main/java/io/github/ololx/cranberry/examples/statement/NotBlankConstructorParamExamples.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2022 the project cranberry-examples authors
+ * and the original author or authors annotated by {@author}
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ololx.cranberry.examples.statement;
+
+import io.github.ololx.cranberry.statement.annotation.NotBlank;
+
+/**
+ * project cranberry-examples
+ * created 2022-02-01 15:57
+ *
+ * @author Alexander A. Kropotin
+ */
+public class NotBlankConstructorParamExamples {
+
+    /**
+     * The type Not blank string param default example.
+     */
+    class NotBlankStringParamDefaultExample {
+
+        private Object obj;
+
+        /**
+         * Instantiates a new Not blank string param default example.
+         *
+         * @param notNullConstructorParam the not null constructor param
+         */
+        NotBlankStringParamDefaultExample(@NotBlank Object notNullConstructorParam) {
+            this.obj = notNullConstructorParam;
+        }
+    }
+
+    /**
+     * The type Not blank string param with message example.
+     */
+    class NotBlankStringParamWithMessageExample {
+
+        private Object obj;
+
+        /**
+         * Instantiates a new Not blank string param with message example.
+         *
+         * @param notNullConstructorParam the not null constructor param
+         */
+        NotBlankStringParamWithMessageExample(
+                @NotBlank(message = "The constructor param is blank, Bro =)")
+                        Object notNullConstructorParam) {
+            this.obj = notNullConstructorParam;
+        }
+    }
+}
